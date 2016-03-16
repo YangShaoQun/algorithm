fe3f7444455f24e5e4925b9e46dc45182056ad81
YangShaoQun <shaoquny@gmail.com>
2016-03-16 17:23:08 +0800

    Highly optimized for general use

--- quicksort/quicksort/main.cpp
+++ quicksort/quicksort/main.cpp
@@ -8,14 +8,16 @@
 
 #include <iostream>
 
-void swap(int* a, int* b)
+template <typename T>
+void swap(T* a, T* b)
 {
-    int tmp = *a;
+    T tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
-int partition(int arr[], int head, int tail)
+template <typename T>
+int partition(T arr[], int head, int tail)
 {
     int sentry = tail-1;
     for (int i=head; i<sentry; ) {
@@ -30,7 +32,8 @@ int partition(int arr[], int head, int tail)
     return sentry;
 }
 
-void quicksort(int arr[], int head, int tail)
+template <typename T>
+void quicksort(T arr[], int head, int tail)
 {
     if (tail-head < 2) {
         return;
@@ -40,14 +43,52 @@ void quicksort(int arr[], int head, int tail)
     quicksort(arr, sentry+1, tail);
 }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+void test1()
+{
     int arr[10] = {3, 7, 8, 5, 2, 6, 1, 9, 5, 4};
     quicksort(arr, 0, 10);
     for (int i=0; i<10; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+void test2()
+{
+    float arr[10] = {3, 7, 8, 5, 2.3, 6, 1, 9, 5, 4};
+    quicksort(arr, 0, 10);
+    for (int i=0; i<10; i++) {
+        printf("%.2f ", arr[i]);
+    }
+    printf("\n");
+}
+
+void test3()
+{
+    char arr[10] = {'c', 'g', 'h', 'e', 'b', 'f', 'a', 'i', 'e', 'd'};
+    quicksort(arr, 0, 10);
+    for (int i=0; i<10; i++) {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+}
+
+void test4()
+{
+    std::string arr[10] = {"cabin", "git", "hair", "exam", "ball", "fvck", "act", "island", "execute", "dead"};
+    quicksort(arr, 0, 10);
+    for (int i=0; i<10; i++) {
+        printf("%s ", arr[i].c_str());
+    }
+    printf("\n");
+}
+
+int main(int argc, const char * argv[])
+{
+    test1();
+    test2();
+    test3();
+    test4();
     
     return 0;
 }
